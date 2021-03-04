9c062f887f5a8eaebda38425da9538433b09df01
TomPretty <tompretty2@gmail.com>
2021-03-04 21:04:09 +0000

    Update visible stags for harry

--- www/src/engine/game.ts
+++ www/src/engine/game.ts
@@ -1,11 +1,11 @@
 import mapboxgl from "mapbox-gl";
 import { getUsers } from "./api";
 import { isProd } from "./env";
+import { distanceBetweenPoints } from "./geo";
 import Player, { DebugPlayer } from "./player";
 import Stag from "./stag";
 
-const UPDATE_PLAYER_LOCATION_INTERVAL_MS = isProd ? 1_000 : 15_000;
-const FETCH_STAG_LOCATIONS_INTERVAL_MS = isProd ? 2_000 : 15_000;
+const GAME_LOOP_INTERVAL_MS = isProd ? 2_000 : 10_000;
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoidG9tcHJldHR5IiwiYSI6ImNqenlpZGRweTBoNGEzaHF0cGNobTk4djgifQ.NNH1CBfwaAo8G8xZ8n9N9g";
@@ -14,37 +14,30 @@ class Game {
   map: mapboxgl.Map;
   player: Player;
   stags: Stag[];
-  updatePlayerLocationInterval: NodeJS.Timeout;
-  fetchStagLocationsTimeout: NodeJS.Timeout;
+  gameLoopInterval: NodeJS.Timeout;
 
   constructor(playerId: string, element: string) {
     this.map = this.getMap(element);
     this.player = this.getPlayer(playerId);
-    this.player.marker.addTo(this.map);
-
-    this.updatePlayerLocationInterval = this.startUpdatePlayerLocationInterval();
-
     this.stags = this.getStags(playerId);
-    this.stags.forEach((stag) => stag.marker.addTo(this.map));
-    this.fetchStagLocationsTimeout = this.startFetchStagLocationsInterval();
+    this.gameLoopInterval = this.startGameLoopInterval();
+
+    this.drawPlayer();
   }
 
   static forPlayer(playerId: string, element: string): Game {
     if (playerId === "debug") {
       return new DebugGame(playerId, element);
+    } else if (playerId === "harry") {
+      return new HarryGame(playerId, element);
     } else {
       return new StagGame(playerId, element);
     }
   }
 
-  update(): void {
-    // only show visible visible stags
-  }
-
   end(): void {
     this.map.remove();
-    clearInterval(this.updatePlayerLocationInterval);
-    clearInterval(this.fetchStagLocationsTimeout);
+    clearInterval(this.gameLoopInterval);
   }
 
   protected getMap(element: string): mapboxgl.Map {
@@ -70,27 +63,53 @@ class Game {
     ].filter((stag) => stag.userId != playerId);
   }
 
-  private startUpdatePlayerLocationInterval() {
-    return setInterval(() => {
-      this.player.updateLocation();
-    }, UPDATE_PLAYER_LOCATION_INTERVAL_MS);
+  private startGameLoopInterval() {
+    return setInterval(() => this.gameLoop(), GAME_LOOP_INTERVAL_MS);
   }
 
-  private startFetchStagLocationsInterval() {
-    return setInterval(async () => {
-      const users = await getUsers();
+  private async gameLoop() {
+    await this.updatePlayerLocation();
+    await this.fetchStagLocations();
+    this.updateVisibleStags();
+    this.drawStags();
+    this.drawPlayer();
+  }
 
-      users.forEach((user) => {
-        this.stags.forEach((stag) => {
-          if (user.id === stag.userId) {
-            const lat = parseFloat(user.loc.lat);
-            const lon = parseFloat(user.loc.lon);
+  private updatePlayerLocation() {
+    return this.player.updateLocation();
+  }
 
-            stag.setLocation(lat, lon);
-          }
-        });
+  private async fetchStagLocations() {
+    const users = await getUsers();
+
+    users.forEach((user) => {
+      this.stags.forEach((stag) => {
+        if (user.id === stag.userId) {
+          const lat = parseFloat(user.loc.lat);
+          const lon = parseFloat(user.loc.lon);
+
+          stag.setLocation(lat, lon);
+        }
       });
-    }, FETCH_STAG_LOCATIONS_INTERVAL_MS);
+    });
+  }
+
+  protected updateVisibleStags(): void {
+    return;
+  }
+
+  private drawStags(): void {
+    this.stags.forEach((stag) => {
+      stag.marker.remove();
+      if (stag.isVisible) {
+        stag.marker.addTo(this.map);
+      }
+    });
+  }
+
+  private drawPlayer(): void {
+    this.player.marker.remove();
+    this.player.marker.addTo(this.map);
   }
 }
 
@@ -99,6 +118,24 @@ export default Game;
 export class DebugGame extends Game {
   protected getPlayer(playerId: string): Player {
     return new DebugPlayer(playerId);
+  }
+}
+
+const STAG_VISIBILITY_RADIUS_IN_M = 250;
+
+export class HarryGame extends Game {
+  protected updateVisibleStags(): void {
+    this.stags.forEach((stag) => {
+      if (stag.userId === this.player.playerId) {
+        return;
+      }
+
+      const { lat: lat1, lon: lon1 } = this.player.getLocation();
+      const { lat: lat2, lon: lon2 } = stag.getLocation();
+      const distanceToStag = distanceBetweenPoints(lat1, lon1, lat2, lon2);
+
+      stag.isVisible = distanceToStag <= STAG_VISIBILITY_RADIUS_IN_M;
+    });
   }
 }
 
